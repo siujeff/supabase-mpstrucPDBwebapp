f7880513afe43041c3ca5d674bd120363b274651
JeffSiu <siujeff@outlook.com>
2025-05-21 16:54:40 -0700

    Initial commit

---
+++ src/ProteinViewer.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from 'react'
+import * as NGL from 'ngl'
+
+export default function ProteinViewer({ pdbUrl }) {
+  const stageRef = useRef(null)
+
+  useEffect(() => {
+    const stage = new NGL.Stage(stageRef.current, { backgroundColor: 'white' })
+    stage.loadFile(pdbUrl, { defaultRepresentation: true })
+
+    return () => stage.dispose()
+  }, [pdbUrl])
+
+  return (
+    <div
+      ref={stageRef}
+      style={{ width: '500px', height: '400px', border: '1px solid #ccc', marginTop: '10px' }}
+    />
+  )
+}
